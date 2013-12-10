5486d03b3e358972769f27e369ba4e4ce1e23588
Taylor Otwell <taylorotwell@gmail.com>
2013-12-10 14:07:52 -0600

    work on script.

--- api-update.sh
+++ api-update.sh
@@ -1,1 +1,28 @@
-php sami/sami.php update sami/config.php
+#!/bin/bash
+
+cd /var/www/laravel.com/sami
+
+rm -rf /var/www/laravel.com/sami/build
+rm -rf /var/www/laravel.com/sami/cache
+
+# Run API Docs For Master
+git clone https://github.com/laravel.framework.git /var/www/laravel.com/sami/laravel
+
+php /var/www/laravel.com/sami/sami.php update /var/www/laravel.com/sami/config.php
+
+cp -r /var/www/laravel.com/sami/build/* /var/www/laravel.com/public/api/master
+
+rm -rf /var/www/laravel.com/sami/build
+rm -rf /var/www/laravel.com/sami/cache
+
+# Run API Docs For 4.0
+cd /var/www/laravel.com/sami/laravel
+git checkout -b 4.0 origin/4.0
+cd ..
+
+php /var/www/laravel.com/sami/sami.php update /var/www/laravel.com/sami/config.php
+
+cp -r /var/www/laravel.com/sami/build/* /var/www/laravel.com/public/api/master
+
+rm -rf /var/www/laravel.com/sami/build
+rm -rf /var/www/laravel.com/sami/cache
