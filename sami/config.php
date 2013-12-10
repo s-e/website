4c994122a051bc5722557978f216ddd77e05c173
Taylor Otwell <taylorotwell@gmail.com>
2013-12-10 13:43:50 -0600

    added sami.

---
+++ sami/config.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+use Sami\Version\GitVersionCollection;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('stubs')
+    ->in($dir = __DIR__.'/laravel/src');
+
+return new Sami($iterator, array(
+    'title'                => 'Laravel API',
+    'build_dir'            => __DIR__.'/build/%version%',
+    'cache_dir'            => __DIR__.'/cache/%version%',
+    'default_opened_level' => 2,
+));
