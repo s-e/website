79761664b60c444607ec2dcdb385d2011ae24d40
Taylor Otwell <taylorotwell@gmail.com>
2013-12-11 14:42:25 -0600

    add 4.1 api generation.

--- api-update.sh
+++ api-update.sh
@@ -15,6 +15,18 @@ cp -r /var/www/laravel.com/sami/build/* /var/www/laravel.com/public/api/master
 rm -rf /var/www/laravel.com/sami/build
 rm -rf /var/www/laravel.com/sami/cache
 
+# Run API Docs For 4.1
+cd /var/www/laravel.com/sami/laravel
+git checkout -b 4.1 origin/4.1
+cd ..
+
+php /var/www/laravel.com/sami/sami.php update /var/www/laravel.com/sami/config.php
+
+cp -r /var/www/laravel.com/sami/build/* /var/www/laravel.com/public/api/4.1
+
+rm -rf /var/www/laravel.com/sami/build
+rm -rf /var/www/laravel.com/sami/cache
+
 # Run API Docs For 4.0
 cd /var/www/laravel.com/sami/laravel
 git checkout -b 4.0 origin/4.0
